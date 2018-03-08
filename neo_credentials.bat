d6917ad7b5a95e4f0006c533a1c472a1742c0d09
A <none>
2018-03-08 12:31:46 +0200

    added a template for .creds.bat

---
+++ neo_credentials.bat
@@ -0,0 +1,13 @@
+@rem TODO put your own credentials and rename to .creds.bat
+
+@set hcp_host=hanatrial.ondemand.com 
+@set hcp_accnt=p0123456789trial
+@set hcp_app=minjerseyhcp
+@set hcp_user=p0123456789 
+@set hcp_pwd=65de2977205c56e309f496cc446ba441d862db94cb18403bbc107264b8eccf4b
+
+@set hcp_tools=D:\Programs\neo-java-web-sdk-3.48.12.1\tools
+@set hcp_src=./target/MinJersey-1.0-SNAPSHOT.war
+
+@rem Home/SAP Cloud Platform/Tools/Console Client/Console Client Commands
+@rem https://help.sap.com/viewer/65de2977205c403bbc107264b8eccf4b/Cloud/en-US/56e309f496cc446ba441d862db94cb18.html
